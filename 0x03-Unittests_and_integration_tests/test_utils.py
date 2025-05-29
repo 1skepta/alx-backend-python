2842ae0aa8cd3d96754dd44cafb80556c33a213f
1skepta <prospaul999@gmail.com>
2025-05-29 08:18:40 +0000

    working

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -54,7 +54,9 @@ class TestGetJson(unittest.TestCase):
     )
     @patch("utils.requests.get")
     def test_get_json(self, test_url, test_payload, mock_get):
-
+        """
+        Test that get_json fetches and returns JSON data from a URL.
+        """
         mock_response = Mock()
         mock_response.json.return_value = test_payload
         mock_get.return_value = mock_response
